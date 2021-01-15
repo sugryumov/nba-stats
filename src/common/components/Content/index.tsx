f38da9b217e4e40d6f6aecb80aac234a0eaff89f
Sergey <snugryumov@gmail.com>
2021-01-15 21:31:38 +0300

    added PUBLIC_URL

--- src/common/components/Content/index.tsx
+++ src/common/components/Content/index.tsx
@@ -3,12 +3,22 @@ import { Route, RouteProps, Switch } from 'react-router-dom';
 import { pages } from './constants';
 
 const Content = () => {
-  console.log('window.location.pathname', window.location.pathname);
+  console.log('window.location.pathname', process.env.PUBLIC_URL);
   return (
     <Switch>
-      {pages.map((props: RouteProps, idx) => (
-        <Route {...props} key={`route_${idx}`} basename="/nba-stats" />
-      ))}
+      {pages.map((props: RouteProps, idx) => {
+        const { path } = props;
+
+        console.log(`${process.env.PUBLIC_URL}${path}`);
+
+        return (
+          <Route
+            {...props}
+            path={`${process.env.PUBLIC_URL}${path}`}
+            key={`route_${idx}`}
+          />
+        );
+      })}
     </Switch>
   );
 };
