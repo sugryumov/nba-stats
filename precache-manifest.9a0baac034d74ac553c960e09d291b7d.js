fd64bbf10648beec2765b250cc3d71991baad203
sugryumov <sugryumov@users.noreply.github.com>
2021-01-15 17:45:48 +0000

    deploy: 645b3cd81ed88b90a1040a116406465f49086b90

---
+++ precache-manifest.9a0baac034d74ac553c960e09d291b7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cb19f83b3bd579dbd37d59a23d4fd42",
+    "url": "/nba-stats/index.html"
+  },
+  {
+    "revision": "0b17c5d25c3e3bebdacc",
+    "url": "/nba-stats/static/css/main.a9149782.chunk.css"
+  },
+  {
+    "revision": "baee5c66ab375208a1f6",
+    "url": "/nba-stats/static/js/2.172474ba.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/nba-stats/static/js/2.172474ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b17c5d25c3e3bebdacc",
+    "url": "/nba-stats/static/js/main.ef4b68a5.chunk.js"
+  },
+  {
+    "revision": "deffed76a64051a5cb39",
+    "url": "/nba-stats/static/js/runtime-main.a9d0ec99.js"
+  }
+]);
