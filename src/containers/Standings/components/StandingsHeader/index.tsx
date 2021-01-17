cb6e344a756224ca1d87b4657462914d79e849ce
Sergey Ugryumov <snugryumov@gmail.com>
2021-01-17 13:21:06 +0300

    styles standing header

--- src/containers/Standings/components/StandingsHeader/index.tsx
+++ src/containers/Standings/components/StandingsHeader/index.tsx
@@ -5,17 +5,47 @@ import { ITheme } from 'interfaces/theme';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Select from '@material-ui/core/Select';
+import Typography from '@material-ui/core/Typography';
 import { groupByList } from 'containers/Standings/constants';
 
 const useStyles = makeStyles((theme: ITheme) => ({
   container: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     padding: 20,
     marginBottom: 32,
 
-    [theme.breakpoints.down('xs')]: {},
+    [theme.breakpoints.down('xs')]: {
+      flexDirection: 'column',
+      alignItems: 'flex-start',
+    },
+  },
+  title: {
+    [theme.breakpoints.down('xs')]: {
+      marginBottom: 20,
+      fontSize: 15,
+    },
   },
   formControl: {
-    minWidth: 120,
+    'minWidth': 150,
+    'height': 40,
+
+    '& .MuiInputBase-root': {
+      height: 40,
+    },
+
+    '& .MuiInputLabel-root': {
+      fontSize: 12,
+    },
+
+    '& .MuiSelect-select:focus': {
+      background: 'transparent',
+    },
+
+    [theme.breakpoints.down('xs')]: {
+      minWidth: '100%',
+    },
   },
 }));
 
@@ -24,6 +54,10 @@ const StandingsHeader = ({ groupBy, handleGroupBy }) => {
 
   return (
     <Paper className={classes.container}>
+      <Typography variant="h6" className={classes.title}>
+        '20 - '21 Regular Season Standings
+      </Typography>
+
       <FormControl variant="outlined" className={classes.formControl}>
         <InputLabel htmlFor="group-by-select">GROUP BY</InputLabel>
         <Select
