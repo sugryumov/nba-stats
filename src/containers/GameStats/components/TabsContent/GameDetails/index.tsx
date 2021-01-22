390c0ea780e48fb921f7656c622e97a961e83fef
Sergey Ugryumov <snugryumov@gmail.com>
2021-01-22 18:59:00 +0300

    fix deploy

--- src/containers/GameStats/components/TabsContent/GameDetails/index.tsx
+++ src/containers/GameStats/components/TabsContent/GameDetails/index.tsx
@@ -1,9 +1,9 @@
-import { getTeamStats } from 'common/selectors/GameStats/getBoxScore';
 import React from 'react';
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
+// import { getTeamStats } from 'common/selectors/GameStats/getBoxScore';
 
 const GameDetails = () => {
-  const gameDetails = useSelector(getTeamStats);
+  // const gameDetails = useSelector(getTeamStats);
 
   return <div>hello</div>;
 };
