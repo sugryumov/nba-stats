d6768772658dc8b0720da5664e6e5d9da33086d4
sugryumov <sugryumov@users.noreply.github.com>
2021-01-30 21:20:46 +0000

    deploy: 91c00a053cb45adb45c9d6480631f5491c48b14c

---
+++ precache-manifest.7b4cd08faf6ef011dfc49993b832a373.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eb63eab61d61a771a5181d19b89b709",
+    "url": "/nba-stats/index.html"
+  },
+  {
+    "revision": "961d2e5036decfd784bf",
+    "url": "/nba-stats/static/css/main.dbcdd037.chunk.css"
+  },
+  {
+    "revision": "24d4079e04b2d32ac728",
+    "url": "/nba-stats/static/js/2.5dd342ab.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/nba-stats/static/js/2.5dd342ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5c2024ab207e60b799b",
+    "url": "/nba-stats/static/js/3.f87f2279.chunk.js"
+  },
+  {
+    "revision": "961d2e5036decfd784bf",
+    "url": "/nba-stats/static/js/main.10d17321.chunk.js"
+  },
+  {
+    "revision": "18c6e684dd8baadbe695",
+    "url": "/nba-stats/static/js/runtime-main.70919e96.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nba-stats/precache-manifest.0c451ed80ee228fd91bd9bdcc9d766ab.js"
+  "/nba-stats/precache-manifest.7b4cd08faf6ef011dfc49993b832a373.js"
 );
 
 self.addEventListener('message', (event) => {
