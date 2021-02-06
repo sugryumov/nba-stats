3d6af9204461b83ba36f59c0be533ade1931c7f6
sugryumov <sugryumov@users.noreply.github.com>
2021-02-06 13:28:42 +0000

    deploy: 4e2a17684f4b6930a9b956c99dc17b76c4178c7f

--- precache-manifest.5e55fd581f3072f821a1c326d9156ce3.js
+++ precache-manifest.5e55fd581f3072f821a1c326d9156ce3.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c6a690d7f2deffb6581e1e2946a0465f",
+    "revision": "4c2ee1f4423abc31eaccbbce15e422a1",
     "url": "/nba-stats/index.html"
   },
   {
-    "revision": "adfeae83724fb2a8d035",
+    "revision": "b7b5c666c26b14f3db80",
     "url": "/nba-stats/static/css/main.dbcdd037.chunk.css"
   },
   {
-    "revision": "0298220c4f0393f11143",
-    "url": "/nba-stats/static/js/2.f68d9129.chunk.js"
+    "revision": "9b3f6beb98db8b39ce2d",
+    "url": "/nba-stats/static/js/2.68b665d0.chunk.js"
   },
   {
     "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
-    "url": "/nba-stats/static/js/2.f68d9129.chunk.js.LICENSE.txt"
+    "url": "/nba-stats/static/js/2.68b665d0.chunk.js.LICENSE.txt"
   },
   {
     "revision": "e5c2024ab207e60b799b",
     "url": "/nba-stats/static/js/3.f87f2279.chunk.js"
   },
   {
-    "revision": "adfeae83724fb2a8d035",
-    "url": "/nba-stats/static/js/main.ccc248ac.chunk.js"
+    "revision": "b7b5c666c26b14f3db80",
+    "url": "/nba-stats/static/js/main.8827dc92.chunk.js"
   },
   {
     "revision": "18c6e684dd8baadbe695",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nba-stats/precache-manifest.08ed00ad56a7e1f87948ba2792922f47.js"
+  "/nba-stats/precache-manifest.5e55fd581f3072f821a1c326d9156ce3.js"
 );
 
 self.addEventListener('message', (event) => {
