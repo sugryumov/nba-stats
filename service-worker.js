b2e7f501a267ffa7e91930fe02bd353d558d0393
sugryumov <sugryumov@users.noreply.github.com>
2021-02-10 05:35:43 +0000

    deploy: ed7109ee31b72c60e0a37ea9077642986559c554

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/nba-stats/precache-manifest.6b67eb983744ec333d5abc64d8de79e2.js"
+  "/nba-stats/precache-manifest.8d8cf0264b807ceb50ed5a52b7919490.js"
 );
 
 self.addEventListener('message', (event) => {
