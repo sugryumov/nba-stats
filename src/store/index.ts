8c412f8a3a36ba8b2f7533156399d86d17da7ee1
Sergey Ugryumov <snugryumov@gmail.com>
2021-02-24 18:16:56 +0300

    chere: remove reducer file

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { rootReducer } from 'store/reducer';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import gamesReducer from 'pages/Games/store';
+import gameStatsReducer from 'pages/GameStats/store';
+import gamePreviewReducer from 'pages/GamePreview/store';
+import standingsReducer from 'pages/Standings/store';
+import statsReducer from 'pages/Stats/store';
+import gamePlayerStatisticsReducer from 'pages/PlayerStats/store';
+
+export const rootReducer = combineReducers({
+  games: gamesReducer,
+  gameStats: gameStatsReducer,
+  gamePreview: gamePreviewReducer,
+  standings: standingsReducer,
+  stats: statsReducer,
+  playerStatistics: gamePlayerStatisticsReducer,
+});
 
 export const store = configureStore({
   reducer: rootReducer,
